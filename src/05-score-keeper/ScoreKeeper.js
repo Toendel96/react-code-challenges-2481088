49649e4c39c02d3b83ba82e6cd9eca59c3aa2264
Petter Tondel <petter.toendel@outlook.com>
2023-01-15 13:31:25 +0100

    La til løsning

--- src/05-score-keeper/ScoreKeeper.js
+++ src/05-score-keeper/ScoreKeeper.js
@@ -1,7 +1,14 @@
 import { useEffect, useState } from 'react'
 
+//Notice localStorage.getItem and localStorage.setItem
 export default function ScoreKeeper () {
-  const [score, setScore] = useState(0)
+  const [score, setScore] = useState(parseInt(localStorage.getItem('score')) || 0)
+  useEffect(() => {
+    localStorage.setItem('score', score)
+  }, [score]) //This is the dependency array. It's an array of values that the useEffect hook will watch for changes. 
+  //If any of the values in the array change, the useEffect hook will run again. 
+  //If the array is empty, the useEffect hook will only run once, when the component is first rendered. 
+  //If the array is not provided, the useEffect hook will run every time the component is rendered.
 
   return (
     <div>

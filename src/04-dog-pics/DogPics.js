8ecc0408584992c32cfd31fe1a73e11c8c5c34d0
Petter Tondel <petter.toendel@outlook.com>
2023-01-15 13:22:11 +0100

    La til løsning

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,28 @@
+import React, { useState, useEffect } from 'react'
+
+const getDogPic = async() => {
+  const response = await fetch('https://dog.ceo/api/breeds/image/random')
+  const dog = await response.json()
+  return dog.message
+}
+
 export default function DogPics () {
   // API: https://dog.ceo/dog-api/
+
+  const [dogPic, setDogPic] = useState('')
+
+  //Runs both when component mounts and when state changes
+  // Without useEffect, a random picture will not be loaded when loading/refreshing page
+  //It would only be loaded when button is clicked, so getDogPic() is called
+  //With useEffect, getDogPic() is called when compoenent is created (refresh/page loaded = mounted)
+  useEffect(() => {
+    getDogPic()
+      .then(pic => setDogPic(pic))
+  }, [])
   return (
     <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
+      <img src={dogPic} />
+      <button onClick={async e => setDogPic(await getDogPic())}>🐶</button>
     </div>
   )
 }

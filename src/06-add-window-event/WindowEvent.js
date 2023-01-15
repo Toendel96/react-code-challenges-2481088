7ef6210908defd4b61b2a4721ee108bb29b60e08
Petter Tondel <petter.toendel@outlook.com>
2023-01-15 13:46:23 +0100

    La til løsning

--- src/06-add-window-event/WindowEvent.js
+++ src/06-add-window-event/WindowEvent.js
@@ -1,4 +1,14 @@
-export default function WindowEvent () {
+import React, { useEffect } from 'react'
+
+export default function () {
+useEffect(() => {
+  const doubleClick = () => alert('mouse clicked')
+
+    window.addEventListener('dblclick', doubleClick) //dblclick is a built in window event that checks for double clicks in browser
+
+    return () => window.removeEventListener('dblclick', doubleClick)
+}, [])
+
   return (
     <h2>Window event active</h2>
   )
